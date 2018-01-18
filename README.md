0866c05e4c7e89ff1eafd08eb58b6e783b8e1a0d
Eric Liu <ericliu121187@gmail.com>
2018-01-17 21:52:54 -0500

    remove CRA; add eslint

--- README.md
+++ README.md
@@ -34,10 +34,7 @@ my-app/
     index.html
     favicon.ico
   src/
-    App.css
     App.js
     App.test.js
-    index.css
     index.js
-    logo.svg
 ```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const puppeteer = require('puppeteer');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://example.com');
+
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio,
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+
+  await browser.close();
+})();
