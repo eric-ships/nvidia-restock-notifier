5fba85aebd193960ba7a60057f0de9372208bec4
Eric Liu <ericliu121187@gmail.com>
2018-01-17 19:39:13 -0500

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  border: 5px solid blue;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <AppWrapper>
+        <header>
+          <h1>Nvidia Restock Notifier</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </AppWrapper>
     );
   }
 }
