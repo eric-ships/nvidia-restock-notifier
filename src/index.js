3e7f09cd7dd441e6d773b539f7cb48d7c7be611e
Eric Liu <ericliu121187@gmail.com>
2018-01-27 15:16:42 -0500

    use headless chrome; is1070OutOfStock, is1080TiOutOfStock

--- src/index.js
+++ src/index.js
@@ -1,21 +1,91 @@
-const puppeteer = require('puppeteer');
+const chromeLauncher = require('chrome-launcher');
+const CDP = require('chrome-remote-interface');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+const NVIDIA_1070_URL = 'https://www.nvidia.com/en-us/geforce/products/10series/geforce-gtx-1070-ti/';
+const NVIDIA_1080TI_URL = 'https://www.nvidia.com/en-us/geforce/products/10series/geforce-gtx-1080-ti/';
 
-  await page.goto('https://example.com');
+// Nvidia 1070
+setInterval(
+  (async function() {
+    async function launchChrome() {
+      return await chromeLauncher.launch({
+        chromeFlags: [
+          '--disable-gpu',
+          '--headless',
+        ],
+      });
+    }
+    const chrome = await launchChrome();
+    const protocol = await CDP({
+      port: chrome.port,
+    });
 
-  // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio,
-    };
-  });
+    const {
+      DOM,
+      Page,
+      Runtime,
+    } = protocol;
+    await Promise.all([Page.enable(), Runtime.enable(), DOM.enable()]);
 
-  console.log('Dimensions:', dimensions);
+    Page.navigate({
+      url: NVIDIA_1070_URL,
+    });
 
-  await browser.close();
-})();
+    Page.loadEventFired(async() => {
+      const expression = 'document.querySelector("#s-tab-2").textContent';
+      const result = await Runtime.evaluate({
+        expression,
+      });
+      const is1070OutOfStock = result.result.value.includes('Notify Me');
+
+      console.log({ is1070OutOfStock });
+
+      protocol.close();
+      chrome.kill();
+    });
+  }),
+  5000,
+);
+
+// Nvidia 1080Ti
+setInterval(
+  (async function() {
+    async function launchChrome() {
+      return await chromeLauncher.launch({
+        chromeFlags: [
+          '--disable-gpu',
+          '--headless',
+        ],
+      });
+    }
+    const chrome = await launchChrome();
+    const protocol = await CDP({
+      port: chrome.port,
+    });
+
+    const {
+      DOM,
+      Page,
+      Runtime,
+    } = protocol;
+    await Promise.all([Page.enable(), Runtime.enable(), DOM.enable()]);
+
+    Page.navigate({
+      url: NVIDIA_1080TI_URL,
+    });
+
+    Page.loadEventFired(async() => {
+      const expression = 'document.querySelector("#section1").textContent';
+      const result = await Runtime.evaluate({
+        expression,
+      });
+      const is1080TiOutOfStock = result.result.value.includes('Notify Me');
+
+      console.log({ is1080TiOutOfStock });
+
+      protocol.close();
+      chrome.kill();
+    });
+  }),
+  5000,
+);
