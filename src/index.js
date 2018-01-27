f57a0b7dc6d2678f050ba83bb230d47b5d2fe64d
Eric Liu <ericliu121187@gmail.com>
2018-01-27 15:57:51 -0500

    get Twilio working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const chromeLauncher = require('chrome-launcher');
 const CDP = require('chrome-remote-interface');
+const twilio = require('twilio');
 
+const DENNYS_NUMBER = '+19088124966';
+const ERICS_NUMBER = '+18624853763';
 const NVIDIA_1070_URL = 'https://www.nvidia.com/en-us/geforce/products/10series/geforce-gtx-1070-ti/';
 const NVIDIA_1080TI_URL = 'https://www.nvidia.com/en-us/geforce/products/10series/geforce-gtx-1080-ti/';
 const REFRESH_INTERVAL_IN_SECONDS = 5;
+const TWILIO_NUMBER = '+16469418448';
+
+const accountSid = 'ACc0a686b46b2b76db54f3c5fd64eb898a'; // Your Account SID from www.twilio.com/console
+const authToken = 'f4a939990587aaa19370118a6066f62c';   // Your Auth Token from www.twilio.com/console
+
+const client = new twilio(accountSid, authToken);
 
 // Nvidia 1070
 setInterval(
@@ -40,6 +49,23 @@ setInterval(
         expression,
       });
       const is1070OutOfStock = result.result.value.includes('Notify Me');
+
+      if (!is1070OutOfStock) {
+        const body = '1070 IS NOW IN STOCK ' + NVIDIA_1070_URL;
+
+        client.messages.create({
+          body,
+          to: DENNYS_NUMBER,
+          from: TWILIO_NUMBER,
+        }).then((message) => console.log(`Sent to Denny's phone! ${message.sid}`));
+
+        client.messages.create({
+          body,
+          to: ERICS_NUMBER,
+          from: TWILIO_NUMBER,
+        }).then((message) => console.log(`Sent to Eric's phone! ${message.sid}`));
+      }
+
       const output = is1070OutOfStock
         ?  '1070 is out of stock'
         : '1070 IS IN STOCK!!! 1070 IS IN STOCK!!! 1070 IS IN STOCK!!!';
@@ -88,6 +114,23 @@ setInterval(
         expression,
       });
       const is1080TiOutOfStock = result.result.value.includes('Notify Me');
+
+      if (!is1080TiOutOfStock) {
+        const body = '1080Ti IS NOW IN STOCK ' + NVIDIA_1080TI_URL;
+
+        client.messages.create({
+          body,
+          to: DENNYS_NUMBER,
+          from: TWILIO_NUMBER,
+        }).then((message) => console.log(`Sent to Denny's phone! ${message.sid}`));
+
+        client.messages.create({
+          body,
+          to: ERICS_NUMBER,
+          from: TWILIO_NUMBER,
+        }).then((message) => console.log(`Sent to Eric's phone! ${message.sid}`));
+      }
+
       const output = is1080TiOutOfStock
         ?  '1080Ti is out of stock'
         : '1080Ti IS IN STOCK!!! 1080Ti IS IN STOCK!!! 1080Ti IS IN STOCK!!!';
